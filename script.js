36a8b182a20abd0d7c83b7183bd4a324fd756e1a
ParticlePhysicsGithub <particlephysicsiguess@gmail.com>
2025-01-07 07:48:12 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth Scroll Effect
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Console Easter Egg
+console.log("Welcome to Particle Physics Games! 🚀");
