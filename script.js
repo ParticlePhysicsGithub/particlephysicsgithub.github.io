220e417d1a553439596db24d2736bb695fe61f69
ParticlePhysicsGithub <particlephysicsiguess@gmail.com>
2025-02-01 14:17:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+/* Place your JavaScript in this file */
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".card").forEach(card => {
+        card.addEventListener("click", function () {
+            window.location.href = this.dataset.url;
+        });
+    });
+});
 
